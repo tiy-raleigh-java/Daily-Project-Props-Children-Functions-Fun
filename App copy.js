9dc784a9a2fdc1608896779fdbfc9ccb248ddd2c
newline-power-tools <doug@theironyard.com>
2017-08-14 20:17:18 +0000

    90f01acd-App%20copy.js

---
+++ App copy.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>I am the Navigation Bar</nav>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>I am the Footer</footer>
+    );
+  }
+}
+
+class BaseLayout extends Component {
+  render() {
+    return (
+      <div>This should house Header and Footer components and be able to house any children components.</div>
+    );
+  }
+}
+
+class ParentComponent extends Component {
+  constructor(props){
+    super(props);
+
+    //we are really in a *bind* here.... :)
+    //fix it...
+
+    //state lives here
+    this.state = {
+      whatToSay: "",
+      whatWasSaid: "",
+    }
+  }
+  handleInput(e) {
+    e.preventDefault();
+    //set the state on input change
+    this.setState({whatToSay: this.state.whatToSay});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    //check console to see if firing
+    console.log("fired");
+    //set the state for props and for value (prevents output from appearing when typing)
+    this.setState({whatToSay: this.state.whatToSay, whatWasSaid: this.state.whatToSay});
+    //clear our input by resetting state
+    this.setState({whatToSay: ""});
+
+  }
+  render() {
+    return (
+      <div>Smart Component: I have a function, but something isn't working? I also need to pass that function to the ChildComponent.
+        <div>
+          <input onChange={this.handleInput} type="text" placeholder="Say It, Don't Spray It!" />
+        </div>
+        <div>
+          <ChildComponent onClick={"FILL_ME_IN"}/>
+          <DisplayComponent sayWhat={"FILL_ME_IN"} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class ChildComponent extends Component {
+  render() {
+    return (
+      <div>Dumb Component receiving Props
+        <div>
+          <input type="submit" onClick={this.props.onClick}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class DisplayComponent extends Component {
+  render() {
+    return (
+      <div>{this.props.sayWhat}</div>
+    );
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BaseLayout></BaseLayout>
+        <Header />
+        <ParentComponent />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
